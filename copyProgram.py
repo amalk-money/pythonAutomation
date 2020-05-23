d9f377a7f27f07c444ab5a57795bbf90d6fb1130
amalk-money <amalkmoney1998@gmail.com>
2020-05-23 18:13:30 +0530

    version 5

--- copyProgram.py
+++ copyProgram.py
@@ -4,7 +4,7 @@ line=f.readlines()
 s=''
 s=s.join(line)
 filename='programFile.py'
-for lines in fileinput.FileInput(filename,inplace=1):
+for lines in fileinput.FileInput(filename):
         if '#add' in lines:
                 lines = lines.rstrip()
                 lines=lines.replace(lines, '\n'+s+'\n' )
