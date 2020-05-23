10475c5b2e895f8c2283d6c9b5f1f5a656b7ac51
amalk-money <amalkmoney1998@gmail.com>
2020-05-23 17:45:47 +0530

    version 3

--- copyProgram.py
+++ copyProgram.py
@@ -1,6 +1,8 @@
 import fileinput
 f=open('addFile.txt','r')
 line=f.readlines()
+s=''
+s=s.join(line)
 filename='programFile.py'
 for lines in fileinput.FileInput(filename,inplace=1):
 	if '#add' in lines:
