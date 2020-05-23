cb2ff2ee8058784cdfee0b974ce5f973257405f9
amalk-money <amalkmoney1998@gmail.com>
2020-05-23 18:08:04 +0530

    version 4

--- copyProgram.py
+++ copyProgram.py
@@ -5,6 +5,6 @@ s=''
 s=s.join(line)
 filename='programFile.py'
 for lines in fileinput.FileInput(filename,inplace=1):
-	if '#add' in lines:
-		lines = lines.rstrip()
-		lines=lines.replace(lines, '\n'+s+'\n' )
+        if '#add' in lines:
+                lines = lines.rstrip()
+                lines=lines.replace(lines, '\n'+s+'\n' )
